faeb6e56b6d26cca49b9c83cd40b119df3b9e140
franciscocrespo <francisco.crespo.jimenez@gmail.com>
2018-01-22 00:09:24 -0300

    implementacion del paso forward

--- RedNeuronal.py
+++ RedNeuronal.py
@@ -16,51 +16,54 @@ class RedNeuronal:
                            for i in range(self.__neurons_for_layer[k])]
                           for k in range(self.__k - 1)]
         self.__b = [[random.uniform(0.1, 0.2) for i in range(k)] for k in self.__neurons_for_layer]
+        self.__target = []
 
-    # This method sets the input data to the inputs of the neural network
+    # This method sets the input data to the inputs of the neural network and right outputs for each inputs
     def set_input_data(self, input_data):
-        if len(self.__a[0]) == len(input_data):
-            self.__a[0] = input_data
+        inp = input_data[0]
+        target = input_data[1]
+        if len(self.__a[0]) == len(inp):
+            self.__a[0] = inp
+        if len(self.__a[self.__k - 1]) == len(target):
+            self.__target = target
 
     # This method gets the weights
     def get_weights(self):
         return self.__weights
 
-    def get_weight_by_id(self, k, i, j):
+    def get_weight_by_id(self, i, j, k):
         return self.__weights[k][i][j]
 
-    def get_a_by_id(self, k, i):
-        return self.__a[k][i]
-
-    def get_b_by_id(self, k, i):
-        return self.__b[k][i]
-
     # This method gets the output of the last layer
-    def get_output(self):
+    def get_a(self):
         return self.__a
+
+    def get_a_by_id(self, i, k):
+        return self.__a[k][i]
 
     # This method gets the bias input
     def get_bias(self):
         return self.__b
 
-    # This method gets the output i in the layer k of a neuron
-    def ai_calculate(self, idx, layer_number):
+    def get_b_by_id(self, i, k):
+        return self.__b[k][i]
 
-        ak_i = 0
-        if layer_number == 0:
-            ak_i = self.__input[idx]
-        if layer_number > 0 & layer_number < self.__k:
-            bi = self.__bias[layer_number][idx]
-            sm = sum([self.__a_multiply_weight(layer_number, j, idx) for j in
-                      range(layer_number - 1)])
-            net = bi + sm
-            ak_i = self.__sigmoid_function(net)
+    # This method gets the target outputs
+    def get_target(self):
+        return self.__target
 
-        return ak_i
+    def get_target_by_id(self, i):
+        return self.__target[i]
 
     # This method gets the global outputs of the artificial neural network
     def forward(self):
-        pass
+        k = 1
+        while k < self.__k:
+            for i in range(self.__neurons_for_layer[k]):
+                ak_i = self.__ai_calculate(i, k)
+                self.__set_aki(ak_i, k, i)
+            k += 1
+
 
     # Auxiliary functions
 
@@ -71,3 +74,18 @@ class RedNeuronal:
 
     def __a_multiply_weight(self, k, j, i):
         return self.__a[k - 1][j] * self.__weights[k - 1][j][i]
+
+    # This method gets the output i in the layer k of a neuron
+    def __ai_calculate(self, idx, layer_number):
+        ak_i = []
+        if layer_number > 0 & layer_number <= self.__k:
+            bi = self.__b[layer_number][idx]
+            sm = sum([self.__a_multiply_weight(layer_number, j, idx) for j in
+                      range(layer_number - 1)])
+            net = bi + sm
+            ak_i = self.__sigmoid_function(net)
+
+        return ak_i
+
+    def __set_aki(self, ak_i, k, i):
+        self.__a[k][i] = ak_i
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from RedNeuronal import RedNeuronal
 
 red = RedNeuronal([2, 4, 2])
-ws = red.get_weights()
-a = red.get_output()
-b = red.get_bias()
-print(ws)
-print(a)
-print(b)
 
-red.set_input_data([1, 2])
-print(a)
+red.set_input_data([[1, 2], [2, 2]])
+red.forward()
+a = red.get_a()
+w = red.get_weights()
+
+print('salida {0}'.format(a))
+print('pesos {0}'.format(w))
